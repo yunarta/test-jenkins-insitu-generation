5803dff263dc3530cffdbba1e794b702f99e2704
Yunarta Kartawahyudi <yunarta.kartawahyudi@gmail.com>
2018-06-23 21:12:19 +0800

    A

--- Jenkinsfile.groovy
+++ Jenkinsfile.groovy
@@ -1,34 +1,38 @@
-if (BRANCH_NAME == "master") {
-    pipeline {
-        agent {
-            node {
-                label "java"
+def call() {
+    if (BRANCH_NAME == "master") {
+        pipeline {
+            agent {
+                node {
+                    label "java"
+                }
+            }
+
+            stages {
+                stage("Master") {
+                    steps {
+                        echo "master branch"
+                    }
+                }
             }
         }
-        
-        stages {
-            stage("Master") {
-                steps {
-                    echo "master branch"
+    } else {
+        pipeline {
+            agent {
+                node {
+                    label "java"
+                }
+            }
+
+            stages {
+                stage("Develop") {
+                    steps {
+                        echo "develop branch"
+                    }
                 }
             }
         }
     }
-} else {
-    pipeline {
-        agent {
-            node {
-                label "java"
-            }
-        }
-
-        stages {
-            stage("Develop") {
-                steps {
-                    echo "develop branch"
-                }
-            }
-        }
-    }
-
 }
+
+call()
+

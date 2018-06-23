d588011c1b7a4a6c51cfe67fe1eb69c9b21f0e0a
Yunarta Kartawahyudi <yunarta.kartawahyudi@gmail.com>
2018-06-23 21:05:16 +0800

    Initial commit

---
+++ Jenkinsfile.groovy
@@ -0,0 +1,34 @@
+if (BRANCH_NAME == "master") {
+    pipeline {
+        agent {
+            node {
+                label "java"
+            }
+        }
+        
+        stages {
+            stage("Master") {
+                steps {
+                    echo "master branch"
+                }
+            }
+        }
+    }
+} else {
+    pipeline {
+        agent {
+            node {
+                label "java"
+            }
+        }
+
+        stages {
+            stage("Develop") {
+                steps {
+                    echo "develop branch"
+                }
+            }
+        }
+    }
+
+}
